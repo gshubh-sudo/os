9722a55f62140e73cacb9b0aada3aed1c58c218e
gshubh-sudo <shubh@kth.se>
2021-03-06 23:53:58 +0530

    cd works fine, have to remove prints statements, also printpath should be in line, and have to modify the forking routine

--- shell.c
+++ shell.c
@@ -12,24 +12,46 @@ char *change_dir();
 void executing_code();
 char* my_strcpy();
 int process_launch();
-
+void remove_all_chars(char *str, char c);
+int finder();
 // char *path;
+char* mydir="MTL4xx~$";
 int main(){
 	char* hist[10];
-	char path[150];
-	strcpy(path,"$MTL4xx:/home/shubh");
-	// printf("%s ",path);
+	char inpath[1500];
+	char currpath[2000];
+	char *printpath=(char*)malloc(1500*sizeof(char));
+	if(getcwd(inpath,1500)!=NULL){
+		printf("the current dir is%s\n",inpath );
+	}
+	printf("MTL4xx~$ ");
+	strcpy(currpath,inpath);
+	int exit=1;
+	do{
+		char* ins =read_from_line(currpath);
+		executing_code(ins,currpath);
+		int k=finder(inpath,currpath,printpath);
+		if(k==0){
+			printpath="";
+		}
+		else if(k==-1){
+			printpath=currpath;
+		}
+		else{
+			printpath=currpath+k;	
+		}
+		// printf("%d\n",k );
+		printf("inside the do while loop\n");
+		printf("%s\n",currpath );
+		printf("MTL4xx~$ ");
+		printf("%s\n",printpath );	
+	}while(exit);
 	
-	char* ins =read_from_line(path);
-
-	// printf("you typed : %s",ins );
-	executing_code(ins,path);
-	printf("not prbls\n");
 	return 0;
 }
 
 char* read_from_line(char* path){
-	printf("%s $", path);
+	// printf("%s $", path);
 	char *buffer=NULL;
 
 	size_t bufsize=32;
@@ -45,22 +67,30 @@ char* read_from_line(char* path){
 	return buffer;
 }
 
+void remove_all_chars(char* str, char c){
+	char *pr=str, *pw=str;
+	while(*pr){
+		*pw=*pr++;
+		pw+=(*pw!=c);
+	}
+	*pw='\0';
+	return;
+}
+
+
 char* change_dir(char* buffer, char* path, char* newPath){
 
-
-	// char newPath[(strlen(buffer)+strlen(path))*2];
-	// printf("%d\n",(int)sizeof(newPath) );
-	// printf("%d\n", (int)strlen(path));
-	// printf("%d\n", (int)strlen(buffer));
-
-	char* pathBuffer = (char*)malloc(sizeof(path));
+	printf("here\n");
+	char* pathBuffer = (char*)malloc(1000);
 	// printf("%s\n",path );
 	strcpy(pathBuffer,path);
-	// printf("this is pathbuffer %s\n",pathBuffer );
+	printf("this is pathbuffer %s\n",pathBuffer );
+	printf("this is new path %s\n", newPath);
+	printf("this is currpath %s\n",path);
 
 	char* p=strtok(buffer," ");
 
-	p=strtok(NULL," ");
+	p=strtok(NULL,"\n");
 	if(p==NULL){
 		printf("enter more arguments\n");
 		return path;
@@ -73,29 +103,34 @@ char* change_dir(char* buffer, char* path, char* newPath){
 
 
 	if(strcmp("..",p)==0){
-		// printf("no problem with the slash?\n");
 		char* slash ="/";
-		// printf("problem not here\n");
+		// printf("%s\n",path );
 		p=strtok(path,"/");
 		// printf("%s\n",p);
-
+		// printf("print ho gaya\n");
+		// strcat(newPath,"/");
+		// printf("%s\n",newPath );
 		char *q=strtok(NULL,"/");
 		while(q!=NULL){
 			p[strcspn(p,"\n")]=0;
-			strcat(newPath,p);
 			strcat(newPath,slash);
+			strcat(newPath,p);
+			
 			p=q;
 			// printf("%s\n",p );
 			q=strtok(NULL,"/");
 		}
-
+		// printf("%s\n",newPath );
 	}
 
 	else if(strcmp(".",p)==0){
 		// printf("brrr\n");
+		// printf("%s\n",path );
 		p=strtok(path,"/");
-		strcpy(newPath,p);
-		strcat(newPath,"/");
+		// printf("%s\n",p );
+		strcpy(newPath,"/");
+		// printf("newPath\n");
+		strcat(newPath,p);
 		// printf("%s\n", newPath);
 	}
 
@@ -104,8 +139,8 @@ char* change_dir(char* buffer, char* path, char* newPath){
 		// printf("%s\n",p );
 		if(p[0]=='/'){
 			// printf("this loop\n");
-			char* q=strtok(path,"/");
-			strcpy(newPath,strcat(q,p));
+			// printf("%s\n",p );
+			strcpy(newPath,p);
 			newPath[strcspn(newPath,"\n")]=0;
 			// printf("%s\n",newPath );
 		}
@@ -121,47 +156,88 @@ char* change_dir(char* buffer, char* path, char* newPath){
 		}
 	}
 
-	char npath[strlen(newPath)+50];
+	char npath[1500];
 	memset(npath,0,sizeof(npath));
 	strcpy(npath,newPath);
 	
 
 	char *pixy= strchr(newPath,'/');
-	char *pixy1=pixy;
-	if(pixy!=NULL){
-		pixy++;
+	
+	remove_all_chars(pixy,'\"');
+	DIR* dir=opendir(pixy);
+	if(dir){
+		closedir(dir);
+	}
+	else if(ENOENT==errno){
+		printf("%s directory does not exist\n", pixy);
+		memset(npath,0,sizeof(npath));
+		strcpy(npath,pathBuffer);
+	}
+	else{
+		printf("cant open directory\n");
 	}
 
-	// printf("%s\n",pixy );
-	if(strcmp(newPath,"$MTL4xx:/")!=0){
-		DIR* dir=opendir(pixy);
-		DIR* dir1=opendir(pixy1);
-		if(dir){
-			closedir(dir);
-		}
-		else if(dir1){
-			closedir(dir1);
-		}
-		else if(ENOENT==errno){
-			printf("directory does not exist\n");
-
-			// printf("%d\n",(int)sizeof(npath) );
-			// printf("%d\n", (int)sizeof(pathBuffer));
-			memset(npath,0,sizeof(npath));
-			strcpy(npath,pathBuffer);
-			// printf("%s\n",npath );
-		}
-		else{
-			printf("cant open directory\n");
-		}
-
-	}
+	
 	strcpy(newPath,npath);
-	printf("%s\n",newPath);
-	// printf("problem is not here\n");
+	remove_all_chars(newPath,'\"');
+	// printf("%s\n",newPath);
+	// printf("%s\n",getcwd(s,200) );
 	return newPath;
 }
 
+int finder(char* inpath, char* currpath, char* printpath){
+	// printf("%s\n",inpath );
+	// printf("%s\n",currpath );
+	// printf("%s\n",printpath );
+
+	if(strcmp(inpath,currpath)==0){
+		// printf("this?\n");
+		printpath="";
+		return 0;
+	}
+	int i=1;
+	// printf("%d\n",(int)strlen(currpath) );
+	// printf("%d\n",(int)strlen(inpath) );
+
+	if(strlen(currpath)<strlen(inpath)){
+		// printf("or this?\n");
+		printpath=currpath;
+		return -1;
+	}
+	
+	else{
+		char* s=inpath,*s1=currpath;
+		// printf("%s\n",s );
+		// printf("%s\n",s1 );
+		int n=strlen(inpath);
+		// printf("%d\n", n);
+		int j=0;
+
+		while(j<n){
+
+			if(inpath[j]!=currpath[j]){
+				// printf("%c\n",inpath[j] );
+				i=0;
+			}
+			j++;
+		}
+		if(i){
+			// printf("this executes\n");
+			// printf("%s\n",s1+i );
+			printpath=(s1+i);
+			// printf("%s\n",printpath );
+			return j;
+		}
+		else{
+			printpath=currpath;
+			return -1;
+
+		}
+	}
+
+
+
+}
 
 void executing_code(char *buffer,char *path){
 
@@ -171,12 +247,12 @@ void executing_code(char *buffer,char *path){
 	// printf("bleh\n");
 
 	//tempbuffer stores the buffer as strtok modifies it
-	char* tempBuffer = (char*)malloc(sizeof(buffer));
+	char* tempBuffer = (char*)malloc(1500);
 	// printf("%d\n", (int)sizeof(buffer));
 	strcpy(tempBuffer,buffer);
 
 	//pathBuffer stores the temporary path
-	char* pathBuffer = (char*)malloc(sizeof(path));
+	char* pathBuffer = (char*)malloc(1500);
 	strcpy(pathBuffer,path);
 
 	//finds the command identifier
@@ -210,16 +286,22 @@ void executing_code(char *buffer,char *path){
 	if(strcmp("cd",p)==0){
 		//we store the newpath in newPath
 		// char* newPath=(char*)malloc(120*sizeof(char));
-		char newPath[150];
+		char newPath[1500];
+		printf("this is new path xx  %s\n",newPath);
 		change_dir(tempBuffer,path, newPath);
 		printf("%s\n",newPath );
 		memset(path,0,sizeof(path));
 		strcpy(path,newPath);
+		memset(newPath,0,sizeof(newPath));
+		printf("%s\n",path );
+		char s[1500];
+		chdir(path);
+		printf("%s\n",getcwd(s,200) );
 		return;
 	}
 
 
-	char **args=malloc(150 * sizeof(char *));
+	char **args=malloc(1500 * sizeof(char *));
 	int pos=0;
 	printf("routine started\n");
 	while(p!=NULL){
@@ -234,6 +316,8 @@ void executing_code(char *buffer,char *path){
 	// printf("\n");
 
 }
+
+//modify this function !!!!!!!
 
 int process_launch(char **args){
 	pid_t p1,wpid;
@@ -256,4 +340,5 @@ int process_launch(char **args){
 	}
 	return 1;
 }
+
 
